27c425cef6009c722c25f7d5f1101b24a290cdbc
Pablo Bangueses <pablo@bng5.net>
2018-05-10 19:00:27 -0300

    Corrección en grilla de Home.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,31 +4,28 @@ var burgerClickRegion = document.getElementsByClassName('burger-click-region');
 var header__menu = document.getElementsByClassName('header__menu')[0];
 
 var forEach = function (t, o, r) {
-    if ("[object Object]" === Object.prototype.toString.call(t)) for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t); else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
+  if ("[object Object]" === Object.prototype.toString.call(t)) for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t); else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
 };
 var hamburgers = document.querySelectorAll(".hamburger");
 if (hamburgers.length > 0) {
-    forEach(hamburgers, function (hamburger) {
-        hamburger.addEventListener("click", function () {
-            this.classList.toggle("is-active");
+  forEach(hamburgers, function (hamburger) {
+    hamburger.addEventListener("click", function () {
+      this.classList.toggle("is-active");
+      if (header__menu.classList) {
+        header__menu.classList.toggle("is-open");
+      } else {
+        var classes = header__menu.className.split(' ');
+        var existingIndex = classes.indexOf("is-open");
 
-            if (header__menu.classList) {
-                header__menu.classList.toggle("is-open");
-            } else {
-                var classes = header__menu.className.split(' ');
-                var existingIndex = classes.indexOf("is-open");
-
-                if (existingIndex >= 0)
-                    classes.splice(existingIndex, 1);
-                else
-                    classes.push(className);
-
-                header__menu.className = classes.join(' ');
-            }
-
-
-        }, false);
-    });
+        if (existingIndex >= 0) {
+          classes.splice(existingIndex, 1);
+        } else {
+          classes.push(className);
+        }
+        header__menu.className = classes.join(' ');
+      }
+    }, false);
+  });
 }
 
 // Sticky header
@@ -53,7 +50,7 @@ if (hamburgers.length > 0) {
   tip.className = 'skills-tip';
   for (var i = 0; i < items.length; i++) {
     items.item(i).addEventListener('mouseenter', function(ev) {
-      var title = this.getElementsByClassName('skills-item__title').item(0).firstChild.nodeValue;
+      var title = this.getElementsByClassName('skills-item__title').item(0).firstChild.textContent;
       tip.firstChild.replaceWith(title);
     });
     items.item(i).addEventListener('mouseleave', function(ev) {
